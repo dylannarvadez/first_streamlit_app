c4c364d31a6da8e92a68f7cf0232d1eff753a663
dylannarvadez <45379594+dylannarvadez@users.noreply.github.com>
2023-07-26 16:35:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -30,19 +29,23 @@ streamlit.dataframe(fruits_to_show)
 ## Display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice')
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+## Introducing this structure allows us to separate the code that is loaded once from the code that should be repeated each time a new value is entered.
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) ## take the json text version of the response above and normalize it
+    streamlit.dataframe(fruityvice_normalized) ## output the result as a table
 
+except URLError as e:
+  streamlit.error()
+
+streamlit.write('The user entered ', fruit_choice)
 
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # streamlit.text(fruityvice_response.json())
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-
-## take the json text version of the response above and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-## output the result as a table
-streamlit.dataframe(fruityvice_normalized)
 
 # The requirements.txt file we added in this project tells Streamlit what libraries we plan to use in our project so it can add them in advance.
 
